4bc34f5733fafd29f71e2318e3cba638fb0588db
Henry Hocking <168395666+bruhcouch@users.noreply.github.com>
2026-05-08 12:15:56 +1000

    Reconfigured AdminStats files

---
+++ project/adminstats/src/main/java/com/example/adminstats/model/OccupancySnapshot.java
@@ -0,0 +1,11 @@
+package com.example.adminstats.model;
+
+import java.util.*;
+
+public class OccupancySnapshot {
+    private Long id;
+    private  timestamp
+    private int lotId
+    private int spotsOccupied;
+    private int spotsTotal;
+}
